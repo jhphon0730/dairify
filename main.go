1c05648c2f0f207bd5fb6026a3225960a3c2f21c
jhphon0730 <eeung025@gmail.com>
2025-07-31 14:14:38 +0900

    refactor: config 초기화 및 데이터베이스 연결 로직 개선 - config 설정을 초기화하는 로직 추가 - 데이터베이스 연결 및 스키마 적용 로직 추가

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/jhphon0730/dairify/internal/config"
+	"github.com/jhphon0730/dairify/internal/database"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// config 설정 초기화
+	if c, err := config.LoadConfig(); err != nil || c == nil {
+		log.Fatalln("Failed to load config:", err)
+	}
+
+	// 데이터베이스 연결 및 스키마 적용
+	if d, err := database.NewDB(); err != nil || d == nil {
+		log.Fatalln("Failed to initialize database:", err)
+	}
 }
