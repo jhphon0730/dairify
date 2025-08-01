56fe026e244dd95918647c3a725cec1e08eb10c7
jhphon0730 <eeung025@gmail.com>
2025-08-01 17:09:15 +0900

    refactor: 사용자 핸들러 인터페이스 및 구조체 주석 수정 - UserHandler 인터페이스 설명을 명확히 함 - userHandler 구조체 설명을 명확히 함 - NewUserHandler 함수 주석을 추가함

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,9 +1,14 @@
 package handler
 
-// UserHandler는 사용자 관련 요청을 처리하는 인터페이스입니다.
+// UserHandler 인터페이스는 사용자 관련 핸들러의 메서드를 정의합니다.
 type UserHandler interface {
 }
 
-// userHandler는 UserHandler 인터페이스를 구현하는 구조체입니다.
+// userHandler 구조체는 UserHandler 인터페이스를 구현합니다.
 type userHandler struct {
+}
+
+// NewUserHandler 함수는 UserHandler 인터페이스의 구현체를 반환합니다.
+func NewUserHandler() UserHandler {
+	return &userHandler{}
 }
